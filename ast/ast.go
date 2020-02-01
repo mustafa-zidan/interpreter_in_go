7808f90d9e102fe15ee9838dad835c5472894e74
Mustafa Zidan <mustafa@zidan.me>
2020-02-01 19:40:34 +0100

    add ast package
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

---
+++ ast/ast.go
@@ -0,0 +1,39 @@
+package ast
+
+import "github.com/mustafa-zidan/interpreter_in_go/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statments []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return token.LET
+}
